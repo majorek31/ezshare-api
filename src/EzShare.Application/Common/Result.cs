56f1f9f212b9bb532d94933dd5c09fb627fead31
majorek31 <m4y0r.offical@gmail.com>
2024-10-14 00:01:28 +0200

    feat: add Result class

---
+++ src/EzShare.Application/Common/Result.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace EzShare.Application.Common;
+
+public class Result<T>
+{
+    public bool Succeeded { get; }
+    public T? Data { get; }
+    public string? Error { get; }
+    public HttpStatusCode StatusCode { get; }
+    protected Result(bool succeeded, T? data, string? error, HttpStatusCode httpStatusCode)
+    {
+        Succeeded = succeeded;
+        Data = data;
+        Error = error;
+        StatusCode = httpStatusCode;
+    }
+    
+    public static Result<T> Success(T data, HttpStatusCode httpStatusCode)
+    {
+        return new Result<T>(true, data, null, httpStatusCode);
+    }
+    
+    public static Result<T> Failure(string error, HttpStatusCode httpStatusCode)
+    {
+        return new Result<T>(false, default, error, httpStatusCode);
+    }
+}
